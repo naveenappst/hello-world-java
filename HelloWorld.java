eb4d0b8f017913a83e4baa456e8f84e9ebebce92
Naveen <naveenappasani7@gmail.com>
2023-12-05 16:12:50 +0530

    added 2 newlines

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     System.out.println("Hello Devops!");
     System.out.println("Hello AWS!");
     System.out.println("Hello CodinGrad!");
-    
+    System.out.println("Hello CodeWorks!");
+    System.out.println("Hello Codin!");
   }
 }
